56d0baa4a807c2d9b8738ea98b68858cdbb41ec1
Edwin Tan <etan094@gmail.com>
2023-04-25 11:19:51 -0700

    Working on creating fetch to spotify api.

---
+++ lib/spotify.ts
@@ -0,0 +1,18 @@
+const SPOTIFY_ENDPOINT = `https://accounts.spotify.com/api/token`;
+
+export async function getAccessToken() {
+  const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
+  const response = fetch(SPOTIFY_ENDPOINT, {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${Buffer.from(
+        `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
+      ).toString("base64")}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      grant_type: "refresh_token",
+      refresh_token,
+    }),
+  });
+}
