a00ebc889d22070482e249cfb7de62f66e26acc0
Edwin Tan <etan094@gmail.com>
2023-04-26 08:49:36 -0700

    Used NextResponse.

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,15 +1,20 @@
 import { spotifyApi } from "@/lib/spotify";
+import { NextResponse } from "next/server";
 
-export async function GET(req: { body: { code: unknown; }; }, res: any) {
-  const code = req.body.code
+export async function GET(req: { body: { code: unknown } }, res: any) {
+  const code = req.body.code;
 
-  spotifyApi.authorizationCodeGrant(code).then(data => {
-
-  res.status(200).json({accessToken: data.body.access_token, refreshToken: data.body.refresh_token,
-    expiresIn: data.body.expires_in})
-  }).catch(e => {
-    console.log('e', e)
-    res.sendStatus(400)
-  })
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      NextResponse.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((e) => {
+      console.log("e", e);
+    });
   // return new Response({ name: 'John Doe' });
 }
